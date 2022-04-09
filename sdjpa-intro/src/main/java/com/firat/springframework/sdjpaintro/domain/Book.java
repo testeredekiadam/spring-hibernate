11920ed02708baeaf8247e3446decb0b8fa2727d
testeredekiadam <erkolfirat@gmail.com>
2022-04-09 05:27:04 +0200

    create Book.java

---
+++ sdjpa-intro/src/main/java/com/firat/springframework/sdjpaintro/domain/Book.java
@@ -0,0 +1,33 @@
+package com.firat.springframework.sdjpaintro.domain;
+
+public class Book {
+	private String name;
+	private String isbn;
+	private String publisher;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	public String getPublisher() {
+		return publisher;
+	}
+	
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	
+}
