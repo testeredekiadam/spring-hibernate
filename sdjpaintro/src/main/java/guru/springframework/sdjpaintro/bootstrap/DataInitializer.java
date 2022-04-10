27cbbc516bc23506d2a58b9ec6e56224b59d56cc
testeredekiadam <erkolfirat@gmail.com>
2022-04-10 09:34:59 +0200

    spring jpa

---
+++ sdjpaintro/src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -0,0 +1,37 @@
+package guru.springframework.sdjpaintro.bootstrap;
+
+import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final BookRepository bookRepository;
+
+    public DataInitializer(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Book bookDDD =  new Book("Domain Driven Design", "123", "RandomHouse");
+
+        System.out.println("Id: " + bookDDD.getId());
+
+        Book savedDDD = bookRepository.save(bookDDD);
+
+        System.out.println("Id: " + savedDDD.getId());
+
+
+        Book bookSIA = new Book("Spring In Action", "234234", "oriely");
+        Book savedSIA = bookRepository.save(bookSIA);
+
+        bookRepository.findAll().forEach(book -> {
+            System.out.println("Book Id: " + book.getId());
+            System.out.println("Book Title: " + book.getName());
+        });
+    }
+}
---
+++ sdjpaintro/src/main/java/guru/springframework/sdjpaintro/domain/Book.java
@@ -0,0 +1,77 @@
+package guru.springframework.sdjpaintro.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String isbn;
+    private String publisher;
+
+    public Book(){
+
+    }
+
+    public Book(String name, String isbn, String publisher) {
+        this.name = name;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
